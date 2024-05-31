27770054ab6a3530e865b91216184b6943630469
Shiwen <95031284+Shiwen-Yang@users.noreply.github.com>
2024-05-31 10:59:39 -0400

    .

--- src/Align.py
+++ src/Align.py
@@ -208,5 +208,64 @@ class Op_Riemannian_GD:
 
         return(W)
 
-        
+       
+def GD_RDPG(A,X, L, tol=1e-3):
+
+    n = A.shape[0]
+    M = torch.ones(n, n) - torch.diag(torch.ones(n))
+    M, A, X = M.to(device), A.to(device), X.to(device)
+
+    X = X[:, :2]
+
+    def gradient(A,X,M):
+
+        gd = 2 * torch.matmul(( -torch.mul((M.T+M), (A)) + torch.mul((M.T+M), torch.matmul(X, X.T))), X)
+        gd = gd + L*(-1)*(X < 0)
+        # rowsum_X_ind = (torch.sum(X, axis = 1) > 1) * 1.0
+        # gd = gd + 50*torch.cat([rowsum_X_ind.unsqueeze(0)] * 2, dim=0).T
+
+        return gd
+    
+    def cost_function(A,X,M):
+
+        cost = 0.5 * torch.norm((A - torch.matmul(X, X.T)) * M, p='fro')**2 
+        cost = cost + L*torch.sum(-X[X<0])
+
+        # rowsum_X = torch.sum(X, axis = 1)
+        # cost = cost + 50*torch.sum(rowsum_X[rowsum_X > 1])
+
+        cost = cost.to("cpu")
+        torch.cuda.empty_cache()
+        return cost
+
+
+    b=0.3; sigma=0.1 # Armijo parameters
+    rank = X.shape[1]
+    max_iter = 200*rank
+    t = 0.1
+    Xd=X
+    k=0
+    last_jump=1
+    d = -gradient(A,Xd,M)
+    tol = tol*(torch.norm(d))
+    while (torch.norm(d) > tol) & (last_jump > 1e-16) & (k<max_iter):
+
+        # Armijo
+        while (cost_function(A, Xd+t*d, M) > cost_function(A, Xd, M) - sigma*t*torch.norm(d)**2):
+            t=b*t
+
+        Xd = Xd+t*d
+        last_jump = sigma*t*torch.norm(d)**2
+        t=t/(b)
+        k=k+1
+        d = -gradient(A,Xd,M)
+
+    Xd = Xd.to("cpu")
+    del(M, A, X, d)
+    torch.cuda.empty_cache()
+
+    last_dim = 1 - torch.sum(Xd, axis = 1).unsqueeze(1)
+    aligned_ASE_full = torch.cat((Xd, last_dim), dim = 1)
+
+    return(aligned_ASE_full) 
 
